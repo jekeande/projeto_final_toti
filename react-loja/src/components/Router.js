5cbb483e6bafd509f3aeb956dbe894c6119b1bc9
jekeande <jekeande@gmail.com>
2022-07-28 23:27:51 -0300

    commit

--- react-loja/src/components/Router.js
+++ react-loja/src/components/Router.js
@@ -6,8 +6,7 @@ import {
   Link
 } from "react-router-dom";
 import Home from "./Home";
-import NovoCliente from './NovoCliente.js';
-import Login from "./Login";
+import Pedido from "./Pedido";
 
 export default function Router() {
   return (
@@ -15,13 +14,12 @@ export default function Router() {
         <BrowserRouter>
               <div className="Menu">
                 <Link to="/"><p className="Home">Home</p></Link>
-                <Link to="/login"><p className="buttonLogin">Login</p></Link>
+                <Link to="/pedido"><p className="buttonLogin">Pedido</p></Link>
               </div>
               <div className="centro">
                 <Routes>
                   <Route exact path="/" element={<Home />} />
-                  <Route exact path="/login" element={<Login />} />
-                  <Route exact path="/new" element={<NovoCliente />} />
+                  <Route exact path="/pedido" element={<Pedido />} />
                 </Routes>
               </div>
       </BrowserRouter>
