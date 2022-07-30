0a30c51d259861eec14d3e050cd111e2ee3f7a87
jekeande <jekeande@gmail.com>
2022-07-29 23:48:11 -0300

    commit

--- react-loja/src/components/Cards.js
+++ react-loja/src/components/Cards.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 export default function Cards({produto}) {
 
+
+  const [data, setData] = useState ({})
+
+  const handleChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const URL = "http://localhost:3001/pedido"
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post(URL,data);
+  }
+
     return (
       <div className="Cards">
+      <form onSubmit={handleSubmit}>
+      <input defaultValue={data.id_produto} onChange={handleChange} />
         <div className="img_produto">
             {produto.foto_produto}
         </div>
@@ -14,6 +34,8 @@ export default function Cards({produto}) {
           <p>Tamanho: {produto.tamanho_produto}</p>
           <p>Preco: {produto.valor_produto}</p>
         </div>
+        <button className="carrinho">Carrinho</button>
+      </form>
       </div>
     );
 }
